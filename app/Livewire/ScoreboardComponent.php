627d799c1605c93524b8a108310f044767be0b5d
Theo2801 <theologos.bourloglou@gmail.com>
2024-12-28 20:29:58 +0200

    [feat] Add scoreboard page

--- app/Livewire/ScoreboardComponent.php
+++ app/Livewire/ScoreboardComponent.php
@@ -2,10 +2,33 @@
 
 namespace App\Livewire;
 
+use App\Models\Player;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ScoreboardComponent extends Component
 {
+    public bool $filteredByUser = true;
+
+    public Collection $allPlayers;
+
+    public Collection $userPlayers;
+
+    public function mount()
+    {
+        $this->allPlayers = $this->getPlayers(false);
+        $this->userPlayers = $this->getPlayers(true);
+    }
+
+    public function getPlayers(bool $filterOnlyUserPlayers)
+    {
+        if ($filterOnlyUserPlayers)
+            return Player::where('user_id', Auth::id())->orderBy('wins', 'desc')->take(10)->get();
+
+        return Player::orderBy('wins', 'desc')->take(10)->get();
+    }
+
     public function render()
     {
         return view('livewire.scoreboard-component');
--- resources/views/livewire/scoreboard-component.blade.php
+++ resources/views/livewire/scoreboard-component.blade.php
@@ -1,3 +1,35 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
+<div class="text-white">
+    <h1 class="text-4xl font-bold text-center">SCOREBOARD</h1>
+    <div class="grid grid-cols-6 col-span-1">
+        <h3 class="my-4 text-2xl text-center underline col-span-full">GLOBAL TOP 10</h3>
+        <span class="text-left underline">NAME</span>
+        <span class="text-center underline">TOTAL GAMES</span>
+        <span class="text-center underline">WINS</span>
+        <span class="text-center underline">LOSSES</span>
+        <span class="text-center underline">WIN RATE</span>
+        <span class="text-right underline">HIGHSCORE</span>
+        @foreach ($allPlayers as $player)
+            <span class="text-lg text-left whitespace-nowrap">{{ $player->name }}</span>
+            <span class="text-lg text-center">{{ $player->total_games }}</span>
+            <span class="text-lg text-center">{{ $player->wins }}</span>
+            <span class="text-lg text-center">{{ $player->losses }}</span>
+            <span class="text-lg text-center">{{ round($player->wins / $player->losses, 2) }}</span>
+            <span class="text-lg text-right">{{ $player->highscore }}</span>
+        @endforeach
+        <h3 class="my-4 text-2xl text-center underline col-span-full">USER TOP 10</h3>
+        <span class="text-left underline">NAME</span>
+        <span class="text-center underline">TOTAL GAMES</span>
+        <span class="text-center underline">WINS</span>
+        <span class="text-center underline">LOSSES</span>
+        <span class="text-center underline">WIN RATE</span>
+        <span class="text-right underline">HIGHSCORE</span>
+        @foreach ($userPlayers as $player)
+            <span class="text-lg text-left whitespace-nowrap">{{ $player->name }}</span>
+            <span class="text-lg text-center">{{ $player->total_games }}</span>
+            <span class="text-lg text-center">{{ $player->wins }}</span>
+            <span class="text-lg text-center">{{ $player->losses }}</span>
+            <span class="text-lg text-center">{{ round($player->wins / $player->losses, 2) }}</span>
+            <span class="text-lg text-right">{{ $player->highscore }}</span>
+        @endforeach
+    </div>
 </div>
