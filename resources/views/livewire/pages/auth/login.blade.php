bf38c444ec1984e42dca095d64c8b7792112834d
Theo2801 <theologos.bourloglou@gmail.com>
2025-01-06 22:51:41 +0200

    [feat] Add loading icon on login and register forms

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -56,14 +56,16 @@ new #[Layout('layouts.guest')] class extends Component
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div x-data="{ loading : false }" class="flex items-center justify-end mt-4">
+            <svg x-show="loading" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="mr-auto text-white icon icon-tabler animate-spin icons-tabler-outline icon-tabler-loader-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3a9 9 0 1 0 9 9" /></svg>
+
             @if (Route::has('password.request'))
                 <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}" wire:navigate>
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button x-on:click="loading = true" class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
